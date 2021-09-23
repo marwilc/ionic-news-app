e0eeba45f996c8786ff99ea5562074343f3f6ca5
Marwil Campos <marwilcampos@gmail.com>
2021-09-22 23:23:33 -0400

    :sparkles: change url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiKey: 'c16d110314264d6a6d0d95f25539132f',
-  apiUrl: 'http://api.mediastack.com/v1',
+  apiUrl: 'https://api.mediastack.com/v1',
 };
